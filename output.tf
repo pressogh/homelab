d1611c9466995f3856bd924a26ef694d24df6349
PRESSO <caff1nepill@gmail.com>
2025-09-28 15:50:24 +0900

    code: delete unused files

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "talosconfig" {
-  value     = data.talos_client_configuration.talos.talos_config
-  sensitive = true
-}
-
-output "kubeconfig" {
-  value     = talos_cluster_kubeconfig.talos.kubeconfig_raw
-  sensitive = true
-}
